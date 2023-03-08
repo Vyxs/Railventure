79e2c0f270ea5c3d23d5558eb18c764af44e4fc3
Maxence <maxcha.cv@gmail.com>
2023-03-08 19:33:06 +0100

    add assets

---
+++ core/src/fr/manigames/railventure/api/core/Metric.kt
@@ -0,0 +1,9 @@
+package fr.manigames.railventure.api.core
+
+object Metric {
+
+    const val TILE_SIZE = 16
+    const val CAMERA_VIEWPORT_WIDTH = 16 * TILE_SIZE
+    const val CAMERA_VIEWPORT_HEIGHT = 9 * TILE_SIZE
+
+}
---
+++ core/src/fr/manigames/railventure/api/core/R.kt
@@ -0,0 +1,19 @@
+package fr.manigames.railventure.api.core
+
+object R {
+
+    object Texture {
+        const val RAIL_V: String = "texture/rail/rail-v.png"
+        const val RAIL_H: String = "texture/rail/rail-h.png"
+        const val RAIL_X: String = "texture/rail/rail-x.png"
+        const val RAIL_TOP_LEFT: String = "texture/rail/rail-top-left.png"
+        const val RAIL_TOP_RIGHT: String = "texture/rail/rail-top-right.png"
+        const val RAIL_BOT_LEFT: String = "texture/rail/rail-bot-left.png"
+        const val RAIL_BOT_RIGHT: String = "texture/rail/rail-bot-right.png"
+        const val RAIL_T_BOT: String = "texture/rail/rail-t-bot.png"
+        const val RAIL_T_TOP: String = "texture/rail/rail-t-top.png"
+        const val RAIL_T_LEFT: String = "texture/rail/rail-t-left.png"
+        const val RAIL_T_RIGHT: String = "texture/rail/rail-t-right.png"
+    }
+
+}
