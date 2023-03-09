1fd12baedeb14836c6005c2532736ef65a1b6f08
Maxence <maxcha.cv@gmail.com>
2023-03-09 01:32:56 +0100

    remove unused import

--- core/src/fr/manigames/railventure/common/renderer/DebugRenderer.kt
+++ core/src/fr/manigames/railventure/common/renderer/DebugRenderer.kt
@@ -4,12 +4,10 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Camera
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.PerspectiveCamera
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.Matrix4
-import com.badlogic.gdx.math.Vector3
 import fr.manigames.railventure.api.core.Metric
 import fr.manigames.railventure.api.graphics.renderer.Renderer
 
