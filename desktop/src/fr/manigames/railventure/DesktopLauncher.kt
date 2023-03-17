8c0603eecf501d7f43175000eecd61514dc041a6
Maxence <maxcha.cv@gmail.com>
2023-03-17 19:13:26 +0100

    change resolution and refresh rate based on primary monitor

--- desktop/src/fr/manigames/railventure/DesktopLauncher.kt
+++ desktop/src/fr/manigames/railventure/DesktopLauncher.kt
@@ -1,17 +1,23 @@
 package fr.manigames.railventure
 
+import com.badlogic.gdx.Graphics
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import fr.manigames.railventure.api.core.Metric
+import fr.manigames.railventure.api.core.R
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 object DesktopLauncher {
 
     @JvmStatic
     fun main(arg: Array<String>) {
+        val primary: Graphics.Monitor = Lwjgl3ApplicationConfiguration.getPrimaryMonitor()
+        val desktopMode: Graphics.DisplayMode = Lwjgl3ApplicationConfiguration.getDisplayMode(primary)
         val config = Lwjgl3ApplicationConfiguration()
-        config.setForegroundFPS(60)
-        config.setWindowedMode(1280, 720)
-        config.setTitle("Railventure")
+
+        config.setForegroundFPS(desktopMode.refreshRate)
+        config.setWindowedMode(Metric.GAME_WIDTH.toInt(), Metric.GAME_HEIGHT.toInt())
+        config.setTitle(R.Strings.APP_NAME)
         Lwjgl3Application(Game(), config)
     }
 }
