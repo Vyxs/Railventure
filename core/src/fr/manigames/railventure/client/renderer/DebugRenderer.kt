764a9781f2bcadd4826ce2a712fee7d2b6695853
Maxence <maxcha.cv@gmail.com>
2023-03-30 21:13:23 +0200

    add gpu debug info

--- core/src/fr/manigames/railventure/client/renderer/DebugRenderer.kt
+++ core/src/fr/manigames/railventure/client/renderer/DebugRenderer.kt
@@ -7,8 +7,10 @@ import com.badlogic.gdx.graphics.Camera
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.GlyphLayout
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.graphics.profiling.GLProfiler
 import com.badlogic.gdx.math.Matrix4
 import fr.manigames.railventure.api.component.ComponentType
 import fr.manigames.railventure.api.core.Metric
@@ -29,8 +31,18 @@ class DebugRenderer(
     private val shapeRenderer = ShapeRenderer()
     private val spriteBatch = SpriteBatch()
     private val font = BitmapFont()
+    private val glyphLayout = GlyphLayout(font, "")
+    private val glProfiler = GLProfiler(Gdx.graphics)
     val inputProcessor = DebugInputProcessor()
 
+    init {
+        inputProcessor.setGpuInfoListener {
+            if (it) glProfiler.enable()
+            else glProfiler.disable()
+        }
+        if (inputProcessor.showGpuInfo)
+            glProfiler.enable()
+    }
     override fun setProjectionMatrix(projectionMatrix: Matrix4?) = Unit
 
     fun render() {
@@ -105,74 +117,115 @@ class DebugRenderer(
     }
 
     private fun renderDebugInfo() {
-        val screenWidth = Gdx.graphics.width.toFloat()
-        val screenHeight = Gdx.graphics.height.toFloat()
-        val stringsToRender = mutableListOf<String>()
+        val info = mutableListOf<String>()
+        val offset = 10f
+
+        getCameraInfo(info)
+        getPlayerInfo(info)
+        getGameInfo(info)
+        getGpuInfo(info)
+
+        val strings = info.reversed()
+
+        renderDebugRect(strings, offset)
+
+        spriteBatch.begin()
+        font.color = Color.WHITE
+        strings.forEachIndexed { index, string ->
+            font.draw(spriteBatch, string, offset, offset * 2 + index * offset * 2)
+        }
+        spriteBatch.end()
+    }
+
+    private fun getCameraInfo(info: MutableList<String>) {
         val camWorldPos = PosUtil.getWorldPosition(camera.position.x, camera.position.y)
         val camChunkPos = PosUtil.getChunkPosition(camera.position.x / Metric.TILE_SIZE, camera.position.y / Metric.TILE_SIZE)
-
-        stringsToRender.add("Camera position: ${camera.position.toRoundedString()}")
-        stringsToRender.add("Camera world position: ${(camWorldPos.first).toRoundedString()}, ${(camWorldPos.second).toRoundedString()}")
-        stringsToRender.add("Camera chunk position: ${camChunkPos.first}, ${camChunkPos.second}")
-        stringsToRender.add("Camera direction: ${camera.direction.toRoundedString()}")
-        stringsToRender.add("Camera viewport: ${camera.viewportWidth.toRoundedString()}, ${camera.viewportHeight.toRoundedString()}")
+        info.add("===== Camera =====")
+        info.add("Camera position: ${camera.position.toRoundedString()}")
+        info.add("Camera world position: ${(camWorldPos.first).toRoundedString()}, ${(camWorldPos.second).toRoundedString()}")
+        info.add("Camera chunk position: ${camChunkPos.first}, ${camChunkPos.second}")
+        info.add("Camera direction: ${camera.direction.toRoundedString()}")
+        info.add("Camera viewport: ${camera.viewportWidth.toRoundedString()}, ${camera.viewportHeight.toRoundedString()}")
 
         if (camera is OrthographicCamera) {
             val chunkVisibleHorizontalCount = PosUtil.getChunkVisibleHorizontal(camera.position.x, camera.viewportWidth, camera.zoom)
             val chunkVisibleVerticalCount = PosUtil.getChunkVisibleVertical(camera.position.y, camera.viewportHeight, camera.zoom)
             val chunkVisibleCount = PosUtil.getChunkVisible(camera.position.x.toInt(), camera.position.y.toInt(), camera.viewportWidth, camera.viewportHeight, camera.zoom)
 
-            stringsToRender.add("Camera zoom: ${camera.zoom.toRoundedString()}")
-            stringsToRender.add("Camera chunk visible horizontal: $chunkVisibleHorizontalCount")
-            stringsToRender.add("Camera chunk visible vertical: $chunkVisibleVerticalCount")
-            stringsToRender.add("Camera chunk visible: $chunkVisibleCount")
+            info.add("Camera zoom: ${camera.zoom.toRoundedString()}")
+            info.add("Camera chunk visible horizontal: $chunkVisibleHorizontalCount")
+            info.add("Camera chunk visible vertical: $chunkVisibleVerticalCount")
+            info.add("Camera chunk visible: $chunkVisibleCount")
         }
+    }
 
+    private fun getPlayerInfo(info: MutableList<String>) {
         world?.let {
             world.getEntitiesWithComponents(ComponentType.PLAYER).forEach { entry ->
                 if ((entry.value.first { it.componentType == ComponentType.PLAYER } as PlayerComponent).isHost) {
                     val worldPosition = entry.value.first { it.componentType == ComponentType.WORLD_POSITION } as WorldPositionComponent
                     val moveable = entry.value.first { it.componentType == ComponentType.MOVEABLE } as MoveableComponent
                     val chunkPosition = PosUtil.getChunkPosition(worldPosition.world_x, worldPosition.world_y)
-
-                    stringsToRender.add("Player position: ${worldPosition.world_x.toRoundedString()}, ${worldPosition.world_y.toRoundedString()}")
-                    stringsToRender.add("Player chunk position: ${chunkPosition.first}, ${chunkPosition.second}")
-                    stringsToRender.add("Player speed: ${moveable.speed.toRoundedString()}")
-                    stringsToRender.add("Player velocity: ${moveable.velocity.toRoundedString()}")
-                    stringsToRender.add("Player acceleration: ${moveable.acceleration.toRoundedString()}")
+                    info.add("===== Player =====")
+                    info.add("Player position: ${worldPosition.world_x.toRoundedString()}, ${worldPosition.world_y.toRoundedString()}")
+                    info.add("Player chunk position: ${chunkPosition.first}, ${chunkPosition.second}")
+                    info.add("Player speed: ${moveable.speed.toRoundedString()}")
+                    info.add("Player velocity: ${moveable.velocity.toRoundedString()}")
+                    info.add("Player acceleration: ${moveable.acceleration.toRoundedString()}")
                 }
             }
         }
+    }
 
-        stringsToRender.add("FPS: ${Gdx.graphics.framesPerSecond}")
-        stringsToRender.add("Delta: ${Gdx.graphics.deltaTime.toRoundedString()}")
-        stringsToRender.add("Memory: ${(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024 / 1024}MB")
-        stringsToRender.add("Entities: ${world?.getEntities()?.size ?: 0}")
+    private fun getGameInfo(info: MutableList<String>) {
+        info.add("===== Game =====")
+        info.add("FPS: ${Gdx.graphics.framesPerSecond}")
+        info.add("Delta: ${Gdx.graphics.deltaTime.toRoundedString()}")
+        info.add("Memory: ${(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024 / 1024}MB")
+        info.add("Entities: ${world?.getEntities()?.size ?: 0}")
+    }
 
+    private fun getGpuInfo(info: MutableList<String>) {
+        if (inputProcessor.showGpuInfo) {
+            info.add("===== GPU =====")
+            info.add("GPU: ${Gdx.graphics.glVersion.rendererString}")
+            info.add("GL calls: ${glProfiler.calls}")
+            info.add("Draw calls: ${glProfiler.drawCalls}")
+            info.add("Shader switches: ${glProfiler.shaderSwitches}")
+            info.add("Texture bindings: ${glProfiler.textureBindings}")
+            info.add("Vertex count: ${glProfiler.vertexCount.total.toInt()}")
+            glProfiler.reset()
+        }
+    }
+
+    private fun renderDebugRect(strings: List<String>, offset: Float = 10f) {
+        var width = 0f
+        var height = 0f
+        val screenWidth = Gdx.graphics.width.toFloat()
+        val screenHeight = Gdx.graphics.height.toFloat()
+        strings.forEach {
+            glyphLayout.setText(font, it)
+            width = maxOf(width, glyphLayout.width)
+            height = maxOf(height, glyphLayout.height)
+        }
         shapeRenderer.projectionMatrix = Matrix4().setToOrtho2D(0f, 0f, screenWidth, screenHeight)
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled)
         shapeRenderer.color = Color(0f, 0f, 0f, 0.5f)
-        shapeRenderer.rect(0f, 0f, 250f, stringsToRender.size * 20f + 10f)
+        shapeRenderer.rect(0f, 0f, width + offset * 2, strings.size * (height + offset) - offset)
         shapeRenderer.end()
-
-        spriteBatch.begin()
-
-        font.color = Color.WHITE
-        stringsToRender.forEachIndexed { index, string ->
-            font.draw(spriteBatch, string, 10f, 20f + index * 20f)
-        }
-        spriteBatch.end()
     }
 
     override fun dispose() {
         shapeRenderer.dispose()
         spriteBatch.dispose()
         font.dispose()
+        glProfiler.disable()
     }
 }
 
 class DebugInputProcessor : InputProcessor {
 
+    private var gpuInfoListener: (Boolean) -> Unit = {}
     var showDebug = true
         private set
     var showTileGrid = true
@@ -180,11 +233,19 @@ class DebugInputProcessor : InputProcessor {
 
     var showChunkGrid = true
         private set
+
+    var showGpuInfo = true
+        private set(value) {
+            field = value
+            gpuInfoListener(value)
+        }
+
     override fun keyDown(keycode: Int): Boolean {
         when (keycode) {
             Keys.F1 -> showDebug = !showDebug
             Keys.F2 -> showTileGrid = !showTileGrid
             Keys.F3 -> showChunkGrid = !showChunkGrid
+            Keys.F4 -> showGpuInfo = !showGpuInfo
         }
         return false
     }
@@ -202,5 +263,8 @@ class DebugInputProcessor : InputProcessor {
     override fun mouseMoved(screenX: Int, screenY: Int): Boolean = false
 
     override fun scrolled(amountX: Float, amountY: Float): Boolean = false
+    fun setGpuInfoListener(function: (Boolean) -> Unit) {
+        gpuInfoListener = function
+    }
 
 }
