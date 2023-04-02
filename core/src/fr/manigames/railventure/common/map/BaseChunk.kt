8c15d133184bc955563d8d52637ac7fedf9e9165
Maxence <maxcha.cv@gmail.com>
2023-04-02 18:00:06 +0200

    fix wrong x and y dimension

--- core/src/fr/manigames/railventure/common/map/BaseChunk.kt
+++ core/src/fr/manigames/railventure/common/map/BaseChunk.kt
@@ -7,6 +7,11 @@ import fr.manigames.railventure.api.map.TileLayer
 
 open class BaseChunk(val x: Int, val y: Int) : MapChunk<TileType> {
 
+    /**
+     * The tiles of the chunk
+     * The first dimension is the y position
+     * The second dimension is the x position
+     **/
     private val tiles = Array(MAP_CHUNK_SIZE) { Array(MAP_CHUNK_SIZE) { TileLayer() } }
 
     /**
@@ -18,11 +23,13 @@ open class BaseChunk(val x: Int, val y: Int) : MapChunk<TileType> {
      * @return the tile at the given position
      */
     override fun getTile(x: Int, y: Int, z: Int): TileType {
-        return TileType.values().first { it.code == tiles[x][y][z] }
+        return TileType.values().first { it.code == tiles[y][x][z] }
     }
 
     /**
-     * Get the tiles of the chunk
+     * Get the tiles of the chunk.
+     * The first dimension is the y position
+     * The second dimension is the x position
      *
      * @return the tiles of the chunk
      */
@@ -39,7 +46,7 @@ open class BaseChunk(val x: Int, val y: Int) : MapChunk<TileType> {
      * @param tileType the tile type
      */
     override fun setTile(x: Int, y: Int, z: Int, tileType: TileType) {
-        tiles[x][y][z] = tileType.code
+        tiles[y][x][z] = tileType.code
     }
 
 
@@ -52,12 +59,12 @@ open class BaseChunk(val x: Int, val y: Int) : MapChunk<TileType> {
     override fun setTiles(tiles: Array<Array<TileLayer>>) {
         this.tiles.forEachIndexed { x, column ->
             column.forEachIndexed { y, _ ->
-                this.tiles[x][y] = tiles[x][y]
+                this.tiles[y][x] = tiles[y][x]
             }
         }
     }
 
     override fun getTileWorldPosition(x: Int, y: Int): Pair<Int, Int> {
-        return Pair(this.x * MAP_CHUNK_SIZE + x, this.y * MAP_CHUNK_SIZE + y)
+        return Pair(this.y * MAP_CHUNK_SIZE + y, this.x * MAP_CHUNK_SIZE + x)
     }
 }
