6d016c24c6ac8030221850d45278d2fed8a691a5
Maxence <maxcha.cv@gmail.com>
2023-04-02 17:53:12 +0200

    update procedural map test

--- core/src/fr/manigames/railventure/test/ProceduralMap.kt
+++ core/src/fr/manigames/railventure/test/ProceduralMap.kt
@@ -7,6 +7,8 @@ import fr.manigames.railventure.api.map.generation.OpenSimplexNoise
 import fr.manigames.railventure.client.map.RenderableChunk
 import fr.manigames.railventure.client.map.RenderableMap
 import java.util.concurrent.atomic.AtomicReference
+import kotlin.math.abs
+import kotlin.math.floor
 import kotlin.random.Random
 
 class ProceduralMap(
@@ -17,52 +19,94 @@ class ProceduralMap(
 
     override fun getGenerationProgress(): Float = generationProgress.get()
 
-    private val seed = 45354454L
+    private val seed = 4455L
     private val noise = OpenSimplexNoise(seed)
 
     override fun generate() {
 
-        val mapSize = MAP_CHUNK_SIZE * 10
-        val noiseGrid = mutableListOf(mutableListOf<Int>())
-        var currentProgress = 0f
+        val widthInChunk = 3
+        val heightInChunk = 3
 
-        for (y in 0 until mapSize) {
-            noiseGrid.add(mutableListOf())
-            for (x in 0 until mapSize) {
-                val id = getIdUsingPerlinNoise(x, y)
-                noiseGrid[y].add(id)
-            }
-        }
 
-        // fill of water
-        for (y in 0 until mapSize / MAP_CHUNK_SIZE)
-            for (x in 0 until mapSize / MAP_CHUNK_SIZE)
-                setChunk(x, y, RenderableChunk(x, y).apply {
-                    for (i in 0 until MAP_CHUNK_SIZE)
-                        for (j in 0 until MAP_CHUNK_SIZE)
-                            setTile(i, j, 0, TileType.WATER)
-                })
+        generateDefaultMap(widthInChunk, heightInChunk)
 
-        for (y in 0 until mapSize) {
-            for (x in 0 until mapSize) {
-                setTile(x, y, 0, TileType.fromCode(noiseGrid[y][x]))
-                currentProgress += 1f / (mapSize * mapSize)
-                generationProgress.set(currentProgress)
-            }
-        }
+        for (x in 0 until 64)
+            setTile(x, 0, 0, TileType.GRASS)
+
+        //generateNoiseMap(widthInChunk, heightInChunk)
+
         generationProgress.set(1f)
     }
 
-    private fun getIdUsingPerlinNoise(x: Int, y: Int): Int {
-        val nx = x.toDouble() / (10 * 16) - 0.5
-        val ny = y.toDouble() / (10 * 16) - 0.5
-        val noise = noise.random2D(nx, ny)
-        Logger.info("Noise at $x, $y : $noise")
-        return when {
-            noise < 0 -> TileType.WATER.code
-            noise < 0.1 -> TileType.SAND.code
-            noise < 0.2 -> TileType.DIRT.code
-            else -> TileType.GRASS.code
+    private fun generateNoiseMap(widthInChunk: Int, heightInChunk: Int) {
+        val scale = 1.0
+        val widthInTiles = widthInChunk * MAP_CHUNK_SIZE
+        val heightInTiles = heightInChunk * MAP_CHUNK_SIZE
+
+        val noiseMap = Array(heightInTiles) { IntArray(widthInTiles) }
+
+        for (y in 0 until heightInTiles) {
+            for (x in 0 until widthInTiles) {
+                val nx = x / widthInTiles.toDouble() * 2 - 1
+                val ny = y / heightInTiles.toDouble() * 2 - 1
+
+                noiseMap[y][x] = getNoise(nx, ny, 3)
+            }
         }
+
+        printNoiseMap(noiseMap)
+
+        for (y in 0 until heightInTiles) {
+            for (x in 0 until widthInTiles) {
+
+                val tileType = when (noiseMap[y][x]) {
+                    0 -> TileType.WATER
+                    1 -> TileType.GRASS
+                    2 -> TileType.DIRT
+                    else -> TileType.GRASS
+                }
+                setTile(x, y, 0, tileType)
+            }
+        }
+    }
+
+    private fun printNoiseMap(noiseMap: Array<IntArray>) {
+        val stringBuffer = StringBuffer()
+        for (y in noiseMap.indices) {
+            for (x in noiseMap[y].indices) {
+                val char = when (noiseMap[y][x]) {
+                    0 -> ' '
+                    1 -> '.'
+                    2 -> 'o'
+                    3 -> 'O'
+                    else -> ' '
+                }
+                stringBuffer.append(char)
+            }
+            stringBuffer.append("\n")
+        }
+        println(stringBuffer.toString())
+    }
+
+    private fun generateDefaultMap(width: Int, height: Int) {
+        for (x in -width..width) {
+            for (y in -height..height) {
+                generateDefaultChunk(x, y)
+            }
+        }
+    }
+
+    private fun getNoise(x: Double, y: Double, types: Int) : Int {
+        return floor(abs(noise.random2D(x, y)) * types).toInt()
+    }
+
+    private fun generateDefaultChunk(x: Int, y: Int) {
+        val chunk = RenderableChunk(x, y)
+        for (tileX in 0 until MAP_CHUNK_SIZE) {
+            for (tileY in 0 until MAP_CHUNK_SIZE) {
+                chunk.setTile(tileX, tileY, 0, TileType.WATER)
+            }
+        }
+        setChunk(x, y, chunk)
     }
 }
